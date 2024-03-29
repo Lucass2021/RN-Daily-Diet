3f6bcec0a2d75ad47b45f04190a3c06496968e13
Lucas Dias <lucas.allx@hotmail.com>
2024-03-29 08:58:43 -0300

    feat: adding libs to project

---
+++ dev-dependencies.js
@@ -0,0 +1,13 @@
+// npx create-expo-app --template
+
+// npm i @expo-google-fonts/nunito-sans
+// npm i phosphor-react-native
+// npx expo install react-native-svg
+
+// npm install @react-navigation/native
+// npx expo install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/native-stack
+
+// npx expo install @react-native-async-storage/async-storage
+
+

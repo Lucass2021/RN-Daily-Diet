1e54b2662c9e65d46c5b208cea614dd1e54a12a3
Lucas Dias <lucas.allx@hotmail.com>
2024-03-31 13:55:32 -0300

    fix: borderRadius fix in stats card

--- src/components/StatsCard/index.tsx
+++ src/components/StatsCard/index.tsx
@@ -12,14 +12,18 @@ type Props = {
 export const StatsCard = ({stats, aboutStats, isHealthy}: Props) => {
 
     const [isHealthyStyle, setIsHealthyStyle] = useState('')
+    const [borderRadiusStyle, setBorderRadiusStyle] = useState(0); 
 
     const handleHealthyStyle = () => {
         if(isHealthy === 'disable'){
             setIsHealthyStyle(theme.colors.gray_5);
+            setBorderRadiusStyle(8)
         } else if(isHealthy === 'healthy'){
             setIsHealthyStyle(theme.colors.green_light);
+            setBorderRadiusStyle(8)
         } else if(isHealthy === 'notHealthy'){
             setIsHealthyStyle(theme.colors.red_light);
+            setBorderRadiusStyle(8)
         }
     }
 
@@ -28,7 +32,7 @@ export const StatsCard = ({stats, aboutStats, isHealthy}: Props) => {
     }, [isHealthy])
 
     return (
-        <View style={[styles.container, { backgroundColor: isHealthyStyle, borderRadius: 8 } ]}>
+        <View style={[styles.container, { backgroundColor: isHealthyStyle, borderRadius: borderRadiusStyle } ]}>
             <Text style={styles.stats}>{stats}</Text>
             <Text style={styles.aboutStats}>{aboutStats}</Text>
         </View>
